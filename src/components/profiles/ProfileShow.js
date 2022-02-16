09a67cb9ea2f19d7cdec4daa5ff767190f6fa8bc
mwpenn94 <mwpenn94@gmail.com>
2022-02-16 14:39:12 -0700

    Update About list and profile icons

--- src/components/profiles/ProfileShow.js
+++ src/components/profiles/ProfileShow.js
@@ -13,12 +13,29 @@ class ProfileShow extends React.Component {
     this.props.fetchStarships(this.props.person.url, this.props.person.starships);
   }
 
+  renderAbout() {
+    const { height, mass, hair_color, birth_year } = this.props.person;
+    const about = [`Height: ${height} cm`, `Weight: ${mass} kg`, `Hair Color: ${hair_color}`, `Born: ${birth_year}` ]
+      return about && about.length > 0 && about.map((trait, index) => {
+        return (
+          <div className="item" key={index}>
+            <i className="user circle outline icon"></i>
+            <div className="content">
+                {trait}
+              <div className="description">
+              </div>
+            </div>
+          </div>
+        );
+      });
+    }
+  
   renderSpeciesList() {
     const { speciesWithDetails: species } = this.props.person;
     return species && species.length > 0 && species.map((specie) => {
       return (
         <div className="item" key={specie.id}>
-          <i className="ui mini image" src="/images/wireframe/image.png" />
+          <i class="reddit alien icon"></i>
           <div className="content">
               {specie.name}
             <div className="description">
@@ -34,7 +51,7 @@ class ProfileShow extends React.Component {
     return films && films.length > 0 && films.map((film) => {
       return (
         <div className="item" key={film.id}>
-          <i className="large middle aligned icon camera" />
+          <i className="film icon" />
           <div className="content">
               {film.title}
             <div className="description">
@@ -50,7 +67,7 @@ class ProfileShow extends React.Component {
     return starships && starships.length > 0 && starships.map((starship) => {
       return (
         <div className="item" key={starship.id}>
-          <i className="large middle aligned icon camera" />
+          <i className="space shuttle icon" />
           <div className="content">
               {starship.name}
             <div className="description">
@@ -65,7 +82,6 @@ class ProfileShow extends React.Component {
     if (!this.props.person) {
       return <div>Loading...</div>;
     }
-console.log(this.props)
     return (
       <div>
         <h1>{this.props.person.name}</h1>
@@ -75,11 +91,8 @@ console.log(this.props)
         {
             this.props.person.image ? <h6>A random image of {this.props.person.name} brought to you by Unsplash</h6> : <h6>Unsplash has no images of {this.props.person.name} :'(</h6>
         }
-                  
-        <h5>Height: {this.props.person.height} cm</h5>
-        <h5>Weight: {this.props.person.mass} kg</h5>
-        <h5>Hair Color: {this.props.person.hair_color}</h5>
-        <h5>Birth Year: {this.props.person.birth_year}</h5>
+        <h5>About        
+        <div className="ui celled list">{this.renderAbout()}</div></h5>
         <h5>Species: 
         <div className="ui celled list">{this.renderSpeciesList()}</div></h5>
         <h5>Films:         
