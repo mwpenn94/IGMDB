0c74666e98b69e916e7be1d7e6e098dcff1ca0f6
Michael Penn <73661865+mwpenn94@users.noreply.github.com>
2022-02-18 12:58:22 -0700

    Add files via upload

--- src/apis/people.js
+++ src/apis/people.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({   //create preconfig Axios instance
-    baseURL: 'https://swapi.dev/api/'
+    baseURL: 'https://vbvt6aw43d.execute-api.us-east-1.amazonaws.com/staging'
+    //    baseURL: 'https://swapi.dev/api/'
 })
--- src/apis/unsplash.js
+++ src/apis/unsplash.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-      'Client-ID 9ByZzJOgsSfEZ4J2k1y1CRE8MPcsnS4ehfIACHT9Xts'
-  }
+  baseURL: 'https://9n7vkspz14.execute-api.us-east-1.amazonaws.com/staging',
+//  baseURL: 'https://api.unsplash.com',
+//  headers: {
+//    Authorization:
+//      'Client-ID 9ByZzJOgsSfEZ4J2k1y1CRE8MPcsnS4ehfIACHT9Xts'
+//  }
 });
